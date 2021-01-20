5729b5cdd14b7c4faf075d8796cc851d1f7fa3c9
sampada <55329464+sampada101@users.noreply.github.com>
2021-01-20 18:55:37 +0545

    Update index.js

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ function draw(){
         
         if( bX + bird.width >= pipe[i].x && bX <= pipe[i].x + pipeNorth.width && (bY <= pipe[i].y + pipeNorth.height || bY+bird.height >= pipe[i].y+constant) || bY + bird.height >=  cvs.height - fg.height){
             alert(`Your score is ${score}`)
-            location.assign('https://sampada101.github.io');
+            location.assign('https://sampada101.github.io/Flappy-Bird/reload.html');
         }
         
         if(pipe[i].x == 5){
